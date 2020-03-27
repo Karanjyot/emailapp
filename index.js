0f88b0dbac516d0d69d146a6dad2dc6e51fad648
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-03-27 18:52:12 -0400

    heroku setup

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express");
 // generate app object
 const app = express();
 
-app.get(".", (req,res) =>{
+app.get("/", (req,res) =>{
 
     res.send({  hi: "there"});
 });
 
-app.listen(5000);
+
+const PORT = process.env.PORT || 5000
+
+// listen to port 
+app.listen(PORT);
