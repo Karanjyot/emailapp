e5b70bd2780516780261bf3c6ec2dc841dc36922
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-03-27 20:59:18 -0400

    setup google redirect with code

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ passport.use(new GoogleStrategy({
     console.log(accessToken);
 }));
 
+//when user hits this route we ask passport to authenticate using the strategy called google. Scope allows google to know what permissions we are asking for.
+app.get("/auth/google", passport.authenticate("google",{
+    scope: ["profile", "email"]
+
+})
+);
+//once user is confirm authentication,user is redirected to url below with a code. Passport calls on the google strategy which calls on access token inside GoogleStrategy.
+app.get("/auth/google/callback", passport.authenticate("google"))
 const PORT = process.env.PORT || 5000
 
 // listen to port 
