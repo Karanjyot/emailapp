616878282e7fb14f3410a2412cfe025ccde940df
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-03-27 20:23:56 -0400

    set up passport.use

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const passport = require("passport");
+const keys = require("./config/keys")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 
 // generate app object
 const app = express();
 
-app.get("/", (req,res) =>{
 
-    res.send({  hi: "there"});
-});
-
+// allowing passport to use the new instance of passport strategy and adding 2 arguments
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+},(accessToken) => {
+    console.log(accessToken);
+}));
 
 const PORT = process.env.PORT || 5000
 
