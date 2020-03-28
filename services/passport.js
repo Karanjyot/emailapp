c5ca22d0467bae8b7a31f1c552beb8e08a401f3e
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-03-28 16:44:39 -0400

    save googleid of user to db and retrieve googleid

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,10 @@ const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const mongoose = require("mongoose");
 
+// user class 
 const User = mongoose.model("users");
+
+
 
 // allowing passport to use the new instance of passport strategy and adding 2 arguments
 passport.use(new GoogleStrategy({
@@ -11,6 +14,22 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: "/auth/google/callback"
 },(accessToken, refreshToken, profile, done) => {
-    //creates record using model instance and saves it
-    new User({ googleId: profile.id }).save();
+
+// find googleId of selected user from DB
+    User.findOne({googleId: profile.id})
+        .then((existingUser)=>{
+            if(existingUser) {
+                //we have a record with given profile ID. Done allows passport to know that it needs to continue authentication flow.
+                // 1st argument is an error object. Pass null since we found a user so there is no error. Second argument provides existing user to passport. 
+                done(null, existingUser);
+            }else{
+                //we don't have a user record with this ID. Make a new record
+                //creates record using model instance and saves it
+                new User({ googleId: profile.id }).save()
+                // once user is created, pass user to passport and continue the authentication flow. 
+                .then(user=> done(null,user));
+            }
+        });
+   
 }));
+
